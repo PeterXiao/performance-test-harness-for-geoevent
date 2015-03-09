b436584d31232c13fae63b6c0b0e1fb72293cf0d
Ricardo Trujillo <rtrujillo@esri.com>
2015-03-09 16:16:11 -0700

    Added the copyright text to all Java Files

--- performance-test-harness-api/src/main/java/com/esri/geoevent/test/performance/jaxb/Appliable.java
+++
@@ -1,6 +0,0 @@
-package com.esri.geoevent.test.performance.jaxb;
-
-public interface Appliable<T>
-{
-	void apply(T t);
-}
--- performance-test-harness-api/src/main/java/com/esri/geoevent/test/performance/jaxb/Applicable.java
+++ performance-test-harness-api/src/main/java/com/esri/geoevent/test/performance/jaxb/Applicable.java
@@ -21,39 +21,9 @@
 
   email: contracts@esri.com
  */
-package com.esri.geoevent.test.performance.utils;
+package com.esri.geoevent.test.performance.jaxb;
 
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-
-public class NetworkUtils
+public interface Applicable<T>
 {
-	public static boolean isLocal(String hostName)
-	{
-		try
-		{
-			//get the address
-			InetAddress address = InetAddress.getByName(hostName);
-			
-			// Check if the address is a valid special local or loop back
-			if (address.isAnyLocalAddress() || address.isLoopbackAddress())
-				return true;
-	
-			// Check if the address is defined on any interface
-			try
-			{
-				return NetworkInterface.getByInetAddress(address) != null;
-			}
-			catch (SocketException e)
-			{
-				return false;
-			}
-		} catch(UnknownHostException error)
-		{
-			return false;
-		}
-	}
-	
+	void apply(T t);
 }
